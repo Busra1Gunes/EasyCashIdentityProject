05e1137abafddb866825e443828f1b84861cb079
Büşra Güneş <121101278+Busra1Gunes@users.noreply.github.com>
2024-05-31 21:05:56 +0300

    Entitylerin oluşturulması

---
+++ EasyCashIdentityProjectEntityLayer/Concrete/CustomerAccount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.EntityLayer.Concrete
+{
+    //Eğer access modifier olarak internal kullanılırsa diğer katmanlarda class'a erişim sağlanmaz
+    public  class CustomerAccount
+    {
+        public int CustomerAccountID { get; set; }
+        public string CustomerAccountNumber { get; set; }
+        public string CustomerAccountCurrency { get; set; } //para birimi
+        public string CustomerAccountBalance { get; set; } //bakiye
+        public string BankBranch { get; set; }
+
+    }
+}
---
+++ EasyCashIdentityProjectEntityLayer/Concrete/CustomerAccountProcess.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.EntityLayer.Concrete
+{
+    public class CustomerAccountProcess
+    {
+        //id, islem türü, tutar , tarih ,a1,a2
+        public int CustomerAccountProcessID { get; set; }
+        public int ProcessType { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime ProcessDate { get; set; }
+
+    }
+}
